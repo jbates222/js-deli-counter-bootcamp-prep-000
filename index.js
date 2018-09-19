e6f271a077af2f13cbaa5c410570e76a7517e590
Jackson Bates <jkbates222@gmail.com>
2018-09-19 16:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   var order = katzDeliLine.length + 1;
-  
+  return "Welcome, " + name + ". You are number " + order + " in line.";
 }
